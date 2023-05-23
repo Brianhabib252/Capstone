d8e9c1985ad948dfad671c804b31ff931f0196fc
Brian Habib Putra Purnama <brianhabib252@gmail.com>
2023-05-23 07:33:55 +0700

    Your commit message

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded ({extended: true}))
+const { verifyAccessToken } = require('./helper/jwt_helper')
 
 //const ProductRoute = require('./routes/Product.route');
 const AuthRoute = require('./routes/Auth.route');
+
+app.get('/', verifyAccessToken, async (req, res, next) => {
+    res.send('Hello from express.')
+  })
+
 app.use('/auth', AuthRoute);
 
 app.use((req, res, next) =>{
@@ -26,6 +33,6 @@ app.use((err, req, res, next) =>{
 })
 
 
-app.listen(300, () => {
+app.listen(3000, () => {
     console.log('Server serving in port 3000')
 })
--- routes/Auth.route.js
+++ routes/Auth.route.js
@@ -1,44 +1,86 @@
-const express = require('express')
-const router = express.Router()
-const mysql = require('mysql')
-const bcrypt = require('bcrypt')
-const {signAccessToken} = require ('../helper/jwt_helper')
-const validation = require('../helper/user.validation')
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const { signAccessToken , signRefreshToken } = require('../helper/jwt_helper');
+const validation = require('../helper/user.validation');
+//const user = require('../model/user.model');
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    database: 'capstone',
-    password: ''
+  host: '127.0.0.1',
+  user: 'capstonesql',
+  database: 'capstone',
+  password: 'password'
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err.message);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
 
-})
+router.post('/register', validation.CreateUser, async (req, res, next) => {
+    const { email, password } = req.body;
 
-router.post('/register', validation.CreateUser , async (req, res, next) => {
-    
-    const {email, password} = req.body
-    const hash = await bcrypt.hash(password, 10)
-
-    const query = "INSERT INTO users (email, hash) values (?, ?);"
-
-    connection.query(query, [email, hash], (err, rows, fields) => {
+  const checkQuery = 'SELECT * FROM users WHERE email = ?';
+  connection.query(checkQuery, [email], (err, rows, fields) => {
+    if (err) {
+      res.status(500).send({ message: err.sqlMessage });
+    } else if (rows.length > 0) {
+      res.status(400).send({ message: 'Email already registered' });
+    } else {
+      bcrypt.hash(password, 10, (err, hash) => {
         if (err) {
-            res.status(500).send({message: err.sqlMessage})
+          res.status(500).send({ message: 'Error hashing password' });
         } else {
-            res.send({message: "Insert Successful"})
+          const insertQuery = 'INSERT INTO users (email, hash) VALUES (?, ?)';
+          connection.query(insertQuery, [email, hash], (err, rows, fields) => {
+            if (err) {
+              res.status(500).send({ message: err.sqlMessage });
+            } else {
+              const userId = result.insertId;
+              res.send({ message: 'Registration successful' });
+            }
+          });
         }
-    })
-})
+      });
+    }
+  });
+});
 
-router.post('/login', (req, res, next) => {
-    res.send('product created')
-})
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const query = 'SELECT * FROM users WHERE email = ?';
+
+  connection.query(query, [email], async (err, rows, fields) => {
+    if (err) {
+      res.status(500).send({ message: err.sqlMessage });
+    } else if (rows.length === 0) {
+      res.status(404).send({ message: 'User not found' });
+    } else {
+      const user = rows[0];
+      const isPasswordValid = await bcrypt.compare(password, user.hash);
+
+      if (isPasswordValid) {
+        const accessToken = await signAccessToken(user.id); 
+        const refreshToken = await signRefreshToken(user.id)
+
+    res.send({ accessToken, refreshToken })
+      } else {
+        res.status(401).send({ message: 'Invalid password' });
+      }
+    }
+  });
+});
 
 router.post('/refresh-token', (req, res, next) => {
-    res.send('getting a single product')
-})
+  res.send('getting a single product');
+});
 
 router.delete('/logout', (req, res, next) => {
-    res.send('deleting a single product')
-})
+  res.send('deleting a single product');
+});
 
 module.exports = router;
