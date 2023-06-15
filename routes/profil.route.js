800cb3086d0f21a4553e132b4ccce15ea3fa7a67
Brianhabib252 <128448072+Brianhabib252@users.noreply.github.com>
2023-06-15 22:44:24 +0700

    Update profil.route.js

--- routes/profil.route.js
+++ routes/profil.route.js
@@ -17,7 +17,39 @@ const {
     console.log('Connected to MySQL');
   });
 
-
+  function nutritionCaloriesCalculation(age, height, weight, gender) {
+    let energy;
+    
+    // Daily Calories Needed
+    if (gender === 'male') {
+      energy = Math.round(66 + (13.7 * weight) + (5 * height) - (6.8 * age), 2);
+    } else {
+      energy = Math.round(655 + (9.6 * weight) + (1.8 * height) - (4.7 * age), 2);
+    }
+    
+    // Nutrient Calculation (Gram)
+    const protein = Math.round(((15 / 100) * energy) / 4, 2);
+    const carbo = Math.round(((60 / 100) * energy) / 4, 2);
+    const fat = Math.round(((15 / 100) * energy) / 9, 2);
+    const fiber = age + 10;
+    
+    return { energy, protein, carbo, fat, fiber };
+  }
+  
+  function eachMealNutrition(energy, protein, carbo, fat, fiber) {
+    energy /= 3;
+    protein /= 3;
+    carbo /= 3;
+    fat /= 3;
+    fiber /= 3;
+    
+    return { energy, protein, carbo, fat, fiber };
+  }
+  
+  function appendNutrition(user, nutrition) {
+    user.push(nutrition);
+  }
+  
   // API endpoint to store profile data
   router.post('/', verifyAccessToken, (req, res) => {
     const { height, weight, age, gender } = req.body;
@@ -41,7 +73,9 @@ const {
         console.error(err);
         res.status(500).json({ error: 'Gagal menyimpan data profil' });
       } else {
-        res.json({ message: 'Data profil berhasil disimpan' });
+      const nutrition = nutritionCaloriesCalculation(age, height, weight);
+      const mealNutrition = eachMealNutrition(...Object.values(nutrition));
+      res.json({ message: 'Data Profil Berhasil Disimpan', nutrition: mealNutrition });
       }
     });
   });
@@ -70,7 +104,9 @@ const {
         console.error(err);
         res.status(500).json({ error: 'Gagal Memperbarui Data profil' });
       } else {
-        res.json({ message: 'Data profil berhasil diperbarui' });
+        const nutrition = nutritionCaloriesCalculation(age, height, weight);
+        const mealNutrition = eachMealNutrition(...Object.values(nutrition));
+        res.json({ message: 'Data Profil Berhasil Diperbarui', nutrition: mealNutrition });
       }
     });
   });
