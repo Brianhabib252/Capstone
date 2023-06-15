9c3ce4945bcad32ba7e7d6bf2b29266536828de8
Brianhabib252 <128448072+Brianhabib252@users.noreply.github.com>
2023-06-15 22:46:04 +0700

    Update app.js
    add new video api

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ const fs = require('fs');
 const moment = require('moment');
 const path = require('path');
 const { verifyAccessToken } = require('./helper/jwt_helper')
-const createConnection = require('../helper/init_mysql')
+const createConnection = require('./helper/init_mysql')
 const AuthRoute = require('./routes/Auth.route');
 const ProfilRoute = require('./routes/profil.route')
+const RecomRoute = require('./routes/Recom.route')
 
 const connection = createConnection();
 connection.connect((err) => {
@@ -26,43 +27,23 @@ app.get('/', verifyAccessToken, async (req, res, next) => {
     res.send('Hello from express.')
   })
 
-app.use('/auth', AuthRoute);
-app.use('/profil', ProfilRoute);
-
-app.use((req, res, next) =>{
-    const err = new Error("Not found")
-    err.status = 404
-    next(err)
-})
-
-//Error handler
-app.use((err, req, res, next) =>{
-    res.status(err.status || 500)
-    res.send({
-        error: {
-            status: err.status || 500,
-            message: err.message
-    }
-
-    })
-})
-
 // API endpoint to retrieve nutrition values by IDs
 app.get('/nutrition', (req, res) => {
     const ids = req.query.ids; // Assuming the input IDs are provided as a comma-separated list in the query parameter "ids"
     const idArray = ids.split(','); // Split the IDs into an array
     
-    const query = `SELECT ID, Nama, Protein, Lemak, Karbohidrat, Serat FROM Nutrition_Data WHERE Nama IN (${idArray.map(() => '?').join(',')})`;
+    const query = `SELECT ID, Nama, Energi, Protein, Lemak, Karbohidrat, Serat FROM Nutrition_Data WHERE Nama IN (${idArray.map(() => '?').join(',')})`;
     
     connection.query(query, idArray, (err, result) => {
       if (err) throw err;
   
       if (result.length === 0) {
-        res.status(404).json({ error: 'No nutrition information found for the provided Food' });
+        res.status(404).json({ error: 'Data Nutrisi Dari Makanan Tidak Tersedia' });
       } else {
         const nutritionData = result.map(row => ({
           ID: row.ID,
           Nama: row.Nama,
+          Energi: row.Energi,
           Protein: row.Protein,
           Lemak: row.Lemak,
           Karbohidrat: row.Karbohidrat,
@@ -71,8 +52,8 @@ app.get('/nutrition', (req, res) => {
         res.json(nutritionData);
       }
     });
-  });
-
+});
+  
   const upload = multer();
 
 // Create a new instance of the GCS client
@@ -114,18 +95,96 @@ ImgUpload.uploadToGcs = (req, res, next) => {
   stream.end(req.file.buffer);
 };
 
-
-// API endpoint to upload an image to GCS
+// post image and upload to ml model
 app.post('/upload', upload.single('image'), ImgUpload.uploadToGcs, (req, res) => {
-  if (!req.file) {
-    res.status(400).json({ error: 'No image file provided' });
-  } else {
-    res.json({ imageUrl: req.file.cloudStoragePublicUrl });
-  }
+    if (!req.file) {
+      res.status(400).json({ error: 'No image file provided' });
+    } else {
+      const imageUrl = req.file.cloudStoragePublicUrl;
+      const predictImageEndpoint = 'https://fastapi-model-ml-rezeodju2q-et.a.run.app/predict_image';
+      const predictImageUrl = `${predictImageEndpoint}?url=${encodeURIComponent(imageUrl)}`;
+  
+      // Make a POST request to predict_image endpoint
+      fetch(predictImageUrl, {
+        method: 'POST'
+      })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Failed to retrieve image from URL');
+          }
+        })
+        .then(predictionResult => {
+          const ids = predictionResult.result; // Assuming the prediction result is the input for the nutrition data retrieval
+          const idArray = ids.split(','); // Split the IDs into an array
+  
+          const query = `SELECT ID, Nama, Energi, Protein, Lemak, Karbohidrat, Serat FROM Nutrition_Data WHERE Nama IN (${idArray.map(() => '?').join(',')})`;
+  
+          connection.query(query, idArray, (err, result) => {
+            if (err) throw err;
+  
+            if (result.length === 0) {
+              res.status(404).json({ error: 'Data Nutrisi Dari Makanan Tidak Tersedia' });
+            } else {
+              const nutritionData = result.map(row => ({
+                ID: row.ID,
+                Nama: row.Nama,
+                Energi: row.Energi,
+                Protein: row.Protein,
+                Lemak: row.Lemak,
+                Karbohidrat: row.Karbohidrat,
+                Serat: row.Serat
+              }));
+              res.json({ imageUrl: imageUrl, prediction: predictionResult.result, nutritionData: nutritionData });
+            }
+          });
+        })
+        .catch(error => {
+          res.status(500).json({ error: 'Internal Server Error' });
+        });
+    }
 });
+  
+// API endpoint to get video links
+app.get('/videos', (req, res) => {
+    const query = 'SELECT * FROM videos';
+    
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Gagal mendapatkan data video' });
+      } else {
+        res.json(result);
+      }
+    });
+});
+
+// API endpoint to get meals based on function
+app.use('/meals', RecomRoute);
+app.use('/auth', AuthRoute);
+app.use('/profil', ProfilRoute);
+
+app.use((req, res, next) =>{
+    const err = new Error("Not found")
+    err.status = 404
+    next(err)
+})
+
+//Error handler
+app.use((err, req, res, next) =>{
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+    }
+
+    })
+})
 
 const port = process.env.PORT || 8080;
 // Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-  });
+});
